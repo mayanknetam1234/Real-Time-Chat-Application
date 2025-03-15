660d391c6a47033d9824de42572fc495728e3c7c
mayanknetam1234 <netammayank547@gmail.com>
2025-03-15 07:38:38 +0530

    made change to limit in app.js

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -15,8 +15,8 @@ const PORT=process.env.PORT ||5001 ;
 
 //built middleware
 
-app.use(express.urlencoded({extended:false,limit:"50mb"}))
-app.use(express.json({limit:"50mb"}))
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
 
 //application middleware
 app.use(cookieParser())
