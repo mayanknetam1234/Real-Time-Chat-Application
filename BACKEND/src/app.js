df833ad74b14124d78494dbe090358d465b10249
mayanknetam1234 <netammayank547@gmail.com>
2025-03-15 07:45:34 +0530

    optimized limits in app.js

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -15,8 +15,8 @@ const PORT=process.env.PORT ||5001 ;
 
 //built middleware
 
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 
 //application middleware
 app.use(cookieParser())
